26758b39282f10deb78f06e322faa976363f1c16
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-10-09 22:15:59 +0100

    add currency formatter

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,19 +1,22 @@
+import { currencyFormatter } from "../formatting";
+
 export default function MealItem({id, name, description, price, image, handleAddToCart}) {
+    
     const imageUrl = "http://localhost:3000/"+image;
     return (
-        <li className="meal-item" key={id}>
+        <li className="meal-item">
             <article>
                 <img src={imageUrl} />
                 <div>
                     <h3>{name}</h3>
-                    <p className="meal-item-price">{price}</p>
+                    <p className="meal-item-price">{currencyFormatter.format(price)}</p>
                     <p className="meal-item-description">
                         {description}
                     </p>
                 </div>
-                <div className="meal-item-actions">
+                <p className="meal-item-actions">
                     <button className="button" onClick={() => handleAddToCart(id)}>Add to Cart</button>
-                </div>
+                </p>
             </article>
         </li>
     );
